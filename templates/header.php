414f8e8abfdf5e598367a44b83c1d8cb594b4b90
Daniel Swakman <dswakman@oefenweb.nl>
2015-04-14 08:33:31 +0300

    mobile nav & dialog styling improvements

--- templates/header.php
+++ templates/header.php
@@ -1,38 +1,55 @@
 <!-- Contact forms -->
 <a href="javascript:void(0)" id="book_a_tour_dialog_mask" class="dialog_mask"></a>
-<dialog id="book_a_tour" class="row u-pv60 u-mh40">
+<dialog id="book_a_tour">
   <a href="javascript:void(0)" id="closeDialog" class="u-pinned-topright"><i class="ion ion-ios-close-empty ion-3x u-mr10"></i></a>
-  <div class="col-xs-8 col-xs-offset-2">
-    <h3>BOOK A TOUR</h3>
-    <p>Do you want to use one of Yazane's excellent spaces for one of your events,  inquire here for more information.</p>
-  </div>
-  <div class="col-xs-12 u-mt40">
-    <?php echo do_shortcode( '[contact-form-7 id="32" title="Book A Tour"]' ); ?>
-
+  <div class="dialog-content">
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+        <h3>BOOK A TOUR</h3>
+        <p>Do you want to use one of Yazane's excellent spaces for one of your events,  inquire here for more information.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 u-mt20">
+        <?php echo do_shortcode( '[contact-form-7 id="32" title="Book A Tour"]' ); ?>
+      </div>
+    </div>
   </div>
 </dialog>
 
 <a href="javascript:void(0)" id="contact_us_dialog_mask" class="dialog_mask"></a>
-<dialog id="contact_us" class="row u-pv60 u-mh40">
+<dialog id="contact_us">
   <a href="javascript:void(0)" id="closeDialog" class="u-pinned-topright"><i class="ion ion-ios-close-empty ion-3x u-mr10"></i></a>
-  <div class="col-xs-8 col-xs-offset-2">
-    <h3>CONTACT US</h3>
-    <p>Are you interested in more info about Yazane, or want to join us as a coworker? Contact us here.</p>
-  </div>
-  <div class="col-xs-12">
-    <?php echo do_shortcode( '[contact-form-7 id="139" title="Contact Us"]' ); ?>
+  <div class="dialog-content">
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+        <h3>CONTACT US</h3>
+        <p>Are you interested in more info about Yazane, or want to join us as a coworker? Contact us here.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 u-mt20">
+        <?php echo do_shortcode( '[contact-form-7 id="139" title="Contact Us"]' ); ?>
+      </div>
+    </div>
   </div>
 </dialog>
 
 <a href="javascript:void(0)" id="host_an_event_dialog_mask" class="dialog_mask"></a>
-<dialog id="host_an_event" class="row u-pv60 u-mh40">
+<dialog id="host_an_event">
   <a href="javascript:void(0)" id="closeDialog" class="u-pinned-topright"><i class="ion ion-ios-close-empty ion-3x u-mr10"></i></a>
-  <div class="col-xs-8 col-xs-offset-2">
-    <h3>HOST AN EVENT</h3>
-    <p>Do you want to use one of Yazane's excellent spaces for one of your events? Inquire here for more information.</p>
-  </div>
-  <div class="col-xs-12">
-    <?php echo do_shortcode( '[contact-form-7 id="154" title="Host an Event"]' ); ?>
+  <div class="dialog-content">
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+        <h3>HOST AN EVENT</h3>
+        <p>Do you want to use one of Yazane's excellent spaces for one of your events? Inquire here for more information.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 u-mt20">
+        <?php echo do_shortcode( '[contact-form-7 id="154" title="Host an Event"]' ); ?>
+      </div>
+    </div>
   </div>
 </dialog>
 
