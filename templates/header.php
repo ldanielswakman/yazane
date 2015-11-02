55c5c3ff30f8e469e17f1155cd5459eff6736b51
Daniel Swakman <dswakman@oefenweb.nl>
2015-11-02 16:24:57 +0300

    nav panel into component

--- templates/header.php
+++ templates/header.php
@@ -53,18 +53,4 @@
   </div>
 </dialog>
 
-<nav>
-  <a href="javascript:void(0)" id="nav-menu-toggle">
-    <i class="ion-navicon ion-2x"></i>
-    <i class="ion-close"></i>
-  </a>
-  <a href="<?php echo site_url() ?>"><div class="logo"><?php image_tag("logo.png"); ?></div></a>
-
-  <?php wp_nav_menu() ?>
-
-  <div class="actions">
-    <a href="#" class="toggle_book_a_tour btn u-ml30 u-mt20"><i class="ion ion-calendar ion-15x u-mr20"></i>BOOK A TOUR</a><br />
-    <a href="#" class="toggle_contact_us btn u-ml30 u-mt20"><i class="ion ion-email ion-15x u-mr20"></i>CONTACT US</a>
-  </div>
-  <a href="javascript:void(0)" id="mask"></a>
-</nav>
+<?php get_template_part('templates/nav'); ?>
