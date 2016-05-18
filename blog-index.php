49c9f8c98076e4d7d6b1222d65a612400db043b3
Daniel Swakman <dswakman@oefenweb.nl>
2016-05-18 20:12:47 +0300

    initial blog functionality

---
+++ blog-index.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Template Name: Blog Index
+*/
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+
+  <?php
+  // list all blog posts here
+  $args = array(
+    'post_type' => 'post',
+    'post_parent' => '0',
+    'order' => 'ASC',
+    'posts_per_page' => '-1'
+  );
+  $posts_query = new WP_Query($args);
+  ?>
+
+  <?php while ( $posts_query->have_posts() ) : $posts_query->the_post(); ?>
+
+    <article class="u-pv60 <?php echo ($posts_query->current_post > 0) ? 'u-bordertop' : ''; ?>">
+      <div class="row">
+        <div class="col-xs-1 col-md-2">
+          <!-- <i class="ion ion-ios-compose-outline ion-3x c-light"></i> -->
+        </div>
+        <div class="col-xs-11 col-sm-10 col-md-8">
+
+          <a href="<?php the_permalink() ?>" class="u-block">
+            <?php // print "(" . ($posts_query->current_post+1) ." of ". $posts_query->post_count . ")"; ?>
+            <h4><big><?php the_title() ?></big></h4>
+            <p class="c-light u-mb20 u-mt0"><small><?php the_date('d F Y') ?></small></p>
+            <p style="line-height: 1.25em;"><small class="c-medium"><?php the_excerpt('') ?></small></p>
+          </a>
+
+          <a href="<?php the_permalink() ?>" class="btn btn-sm u-mt20"><?php echo function_exists('pll__') ? pll__('read_article') : 'Read article'; ?></a>
+
+        </div>
+      </div>
+    </article>
+
+  <?php endwhile ?>
+
+  <?php if ($posts_query->post_count == 0) :?>
+    <div class="u-pv60">
+      <div class="row">
+        <div class="col-xs-1 col-md-2">
+        </div>
+        <div class="col-xs-11 col-sm-10 col-md-8">
+          <?php echo function_exists('pll__') ? pll__('no_blog_posts_found') : 'No posts'; ?>
+          <!-- There are no blog posts to display at the moment... -->
+        </div>
+      </div>
+    </div>
+  <? endif ?>
+
+  <?php
+  // Reset Query
+  wp_reset_query();
+  ?>
+
+  <?php get_template_part('templates/footer', 'page'); ?>
+
+<?php endwhile; ?>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,15 +1,55 @@
 <?php while (have_posts()) : the_post(); ?>
+
   <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+
+    <section id="page-hero" class="parallax" style="background-image: url('/yazane/wp-content/themes/yazane/assets/img/background-woodpanels.jpg');">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1 u-pv80 u-aligncenter">
+          <h3 class="u-mb20"><?php echo function_exists('pll__') ? pll__('blog') : 'BLOG'; ?></h3>
+          <blockquote><?php the_title(); ?></blockquote>
+        </div>
+      </div>
+    </section>
+
+    <section class="u-pt40 u-pb60">
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 c-light u-pb30">
+          <?php // get_template_part('templates/entry-meta'); ?>
+          <?php the_date(); ?>
+        </div>
+      </div>
+    
+      <div class="row">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+          <?php echo wpautop(get_the_content()); ?>
+        </div>
+      </div>
+
+
+      <div class="row u-mt60">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 c-light">
+          <div class="tags"><?php the_tags('', ''); ?></div>
+        </div>
+      </div>
+
+      <div class="row u-mt60">
+        <div class="col-xs-6 col-sm-5 col-sm-offset-1">
+          <a href="<?php echo get_permalink( get_page_by_title('Blog')) ?>"><i class="ion ion-chevron-left u-mr5"></i> <?php echo function_exists('pll__') ? pll__('blog') : 'BLOG'; ?></a>
+        </div>
+        <div class="col-xs-6 col-sm-5 u-alignright">
+          <?php next_post_link(); ?>
+        </div>
+      </div>
+
+    </section>
+
+    <!-- <footer>
+      <?php // wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer> -->
+    <?php // comments_template('/templates/comments.php'); ?>
   </article>
+
+  <?php get_template_part('templates/footer', 'page'); ?>
+
 <?php endwhile; ?>
