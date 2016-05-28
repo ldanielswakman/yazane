46ef86912c50229d4acc924f5d95dcb34f09a297
Daniel Swakman <dswakman@oefenweb.nl>
2016-05-28 16:56:02 +0300

    added like on Facebook banner

--- index.php
+++ index.php
@@ -17,14 +17,22 @@
 
 </main>
 
-<?php
-// outputs in-page nav menu
-echo '<aside><ul id="page-menu">';
-while ( $homepage_posts->have_posts() ) : $homepage_posts->the_post();
-  $visible = get_post_meta( $post->ID, 'section_nav_visible', true );
-  if ($visible != 'no'):
-    echo '<li><a href="#' . $post->post_name . '"' . $class . '><span class="descr">' . strtolower(get_the_title()) . '</span></a></li>';
-  endif;
-endwhile;
-echo '</ul></aside>';
-?>
+<aside class="aside-pagination"><ul id="page-menu">
+  <?php
+  // outputs in-page nav menu
+  while ( $homepage_posts->have_posts() ) : $homepage_posts->the_post();
+    $visible = get_post_meta( $post->ID, 'section_nav_visible', true );
+    if ($visible != 'no'):
+      echo '<li><a href="#' . $post->post_name . '"' . $class . '><span class="descr">' . strtolower(get_the_title()) . '</span></a></li>';
+    endif;
+  endwhile;
+  ?>
+</ul></aside>
+
+<aside class="floatingaction u-fixed-bottomright isHidden" data-appear-ypos="500" data-disappear-ypos="3000">
+  <a href="https://facebook.com/YazaneIstanbul" target="_blank" class="btn btn-primary btn-lg" style="background-color: #3b5999;">
+    <i class="ion ion-social-facebook u-mr10"></i>
+    Like us on Facebook!
+    <i class="ion ion-chevron-right u-ml10"></i>
+  </a>
+</aside>
